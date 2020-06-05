d041e872149403ce8689ff7095f1a8837c95381a
William Yeung <williamyeung1141998@gmail.com>
2020-06-05 15:10:35 -0400

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by William Yeung on 6/5/20.
+//  Copyright © 2020 William Yeung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
